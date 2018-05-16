3a5571ab2a196cab9531080b285c2894b474d0f1
BETH-A <elizabeth78@me.com>
2018-05-16 18:51:57 -0400

    sequelizedBurger

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,14 @@
-// Sequelize (capital) references the standard library
-
-var Sequelize = require("sequelize");
-
-var sequelize = require("../config/connection.js");
-
-//Create "Burger" model to match DB
-var Burger = sequelize.define("burger", {
-	burgerName: {type: Sequelize.STRING, allowNull: false},
-	devoured: {type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
-	date: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
-},{
-	timestamp: false
-});
-
-// Sync with DB
-BurgersDB.sync();
-
-// Export the database functions for the controller
-module.exports = Burger;
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+	  burger_name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	  },
+	  devoured: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false
+	  }
+	});
+  
+	return Burger
+  };
