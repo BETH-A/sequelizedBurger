fce7d91a83e957d695ef1ae6e2359a3375593933
BETH-A <elizabeth78@me.com>
2018-05-16 19:28:37 -0400

    sequelizedBurger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,15 +1,39 @@
-// Wait to attach handler until the DOM is fully loaded.
-$(function(){
-	$(".devour-burger").on("click", function(event){
-		// Set id based on button data-id attribute
-		var id = $(this).data("id");
-		// Send the PUT request to move burger to devoured list
-		$.ajax("/api/burgers/"+id, {
-			type: "PUT",
-			data: id
-		}).then(function(){
-			// Reload the webpage after update to database
-			location.reload();
-		})
-	});
-});
+
+$(document).ready(function(){
+  
+    //Add Burger
+    $("#addButton").on("click", function(event){
+      var name = $("#burgerName").val().trim();
+  
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: {burgerName: name}
+      }).then(function(){
+        location.reload();
+      });
+    });
+  
+    // Devour Burger
+    $(".devour-burger").on("click", function(event){
+      var id = $(this).data("id");
+  
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: {devoured: true}
+      }).then(function(){
+        location.reload();
+      });
+    });
+  
+    // Remove Burger
+    $(".remove-burger").on("click", function(event){
+      var id = $(this).data("id");
+  
+      $.ajax("/api/burger/" + id, {
+        type: "DELETE",
+      }).then(function(){
+        location.reload();
+      });
+    });
+  
+  });
